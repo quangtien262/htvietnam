6ded1cdc5a348556af756010e3976f7306a670df
TienLQ <quangtienvkt@gmail.com>
2025-10-28 16:56:03 +0700

    OK

--- aio12/resources/js/Pages/Admin/Meeting/index.tsx
+++ aio12/resources/js/Pages/Admin/Meeting/index.tsx
@@ -1109,141 +1109,141 @@ export default function meeting(props: any) {
         },
     ];
 
-    // const pageContent = (
-    //     <div>
+    const pageContent = (
+        <div>
 
-    //         <Form form={form} component={false}>
-    //             <div style={{ marginBottom: 16 }}>
-    //                 {/* confirm delete */}
-    //                 <Modal
-    //                     title="Xác nhận xóa"
-    //                     open={isOpenConfirmDelete}
-    //                     onOk={deletes}
-    //                     onCancel={handleCancelDelete}
-    //                 // confirmLoading={loadingBtnDelete}
-    //                 >
-    //                     <p>
-    //                         Dữ liệu đã xóa sẽ <b>không thể khôi phục</b> lại
-    //                         được <br />{" "}
-    //                         <b>(Số lượng {selectedRowKeys.length})</b>
-    //                     </p>
-    //                 </Modal>
+            <Form form={form} component={false}>
+                <div style={{ marginBottom: 16 }}>
+                    {/* confirm delete */}
+                    <Modal
+                        title="Xác nhận xóa"
+                        open={isOpenConfirmDelete}
+                        onOk={deletes}
+                        onCancel={handleCancelDelete}
+                    // confirmLoading={loadingBtnDelete}
+                    >
+                        <p>
+                            Dữ liệu đã xóa sẽ <b>không thể khôi phục</b> lại
+                            được <br />{" "}
+                            <b>(Số lượng {selectedRowKeys.length})</b>
+                        </p>
+                    </Modal>
 
-    //                 {/* modal confirm export curent */}
-    //                 <Modal
-    //                     title="Xác nhận export excel"
-    //                     open={isOpenConfirmExportExcel}
-    //                     onOk={exportExcel}
-    //                     onCancel={handleCancelExport}
-    //                     confirmLoading={loadingBtnExport}
-    //                 >
-    //                     <p>
-    //                         Xuất dữ liệu ra file excel{" "}
-    //                         <b>
-    //                             (Số lượng{" "}
-    //                             {hasSelected
-    //                                 ? selectedRowKeys.length
-    //                                 : props.pageConfig.total}
-    //                             )
-    //                         </b>
-    //                     </p>
-    //                 </Modal>
+                    {/* modal confirm export curent */}
+                    <Modal
+                        title="Xác nhận export excel"
+                        open={isOpenConfirmExportExcel}
+                        onOk={exportExcel}
+                        onCancel={handleCancelExport}
+                        confirmLoading={loadingBtnExport}
+                    >
+                        <p>
+                            Xuất dữ liệu ra file excel{" "}
+                            <b>
+                                (Số lượng{" "}
+                                {hasSelected
+                                    ? selectedRowKeys.length
+                                    : props.pageConfig.total}
+                                )
+                            </b>
+                        </p>
+                    </Modal>
 
-    //                 {/* modal confirm export all */}
-    //                 <Modal
-    //                     title="Xác nhận export excel"
-    //                     open={isOpenConfirmExportAllExcel}
-    //                     onOk={exportAllDBExcel}
-    //                     onCancel={handleCancelAllExport}
-    //                     confirmLoading={loadingBtnExport}
-    //                 >
-    //                     <p>
-    //                         Xuất tất cả dữ liệu ra file excel{" "}
-    //                         <b>(Số lượng {props.pageConfig.total})</b>
-    //                     </p>
-    //                 </Modal>
+                    {/* modal confirm export all */}
+                    <Modal
+                        title="Xác nhận export excel"
+                        open={isOpenConfirmExportAllExcel}
+                        onOk={exportAllDBExcel}
+                        onCancel={handleCancelAllExport}
+                        confirmLoading={loadingBtnExport}
+                    >
+                        <p>
+                            Xuất tất cả dữ liệu ra file excel{" "}
+                            <b>(Số lượng {props.pageConfig.total})</b>
+                        </p>
+                    </Modal>
 
-    //                 {/* modal form import */}
-    //                 <Modal
-    //                     title="Chọn file cần nhập liệu"
-    //                     open={isOpenConfirmImportExcel}
-    //                     onCancel={handleCancelImport}
-    //                     confirmLoading={loadingBtnExport}
-    //                     footer=""
-    //                 >
-    //                     <Upload {...uploadConfig}>
-    //                         <Button icon={<UploadOutlined />}>
-    //                             Select File
-    //                         </Button>
-    //                     </Upload>
-    //                     <Button
-    //                         type="primary"
-    //                         onClick={handleUpload}
-    //                         loading={uploading}
-    //                         style={{
-    //                             marginTop: 16,
-    //                         }}
-    //                     >
-    //                         {uploading ? "Uploading" : "Start Upload"}
-    //                     </Button>
-    //                     <Button onClick={handleCancelImport}>Hủy</Button>
-    //                 </Modal>
+                    {/* modal form import */}
+                    <Modal
+                        title="Chọn file cần nhập liệu"
+                        open={isOpenConfirmImportExcel}
+                        onCancel={handleCancelImport}
+                        confirmLoading={loadingBtnExport}
+                        footer=""
+                    >
+                        <Upload {...uploadConfig}>
+                            <Button icon={<UploadOutlined />}>
+                                Select File
+                            </Button>
+                        </Upload>
+                        <Button
+                            type="primary"
+                            onClick={handleUpload}
+                            loading={uploading}
+                            style={{
+                                marginTop: 16,
+                            }}
+                        >
+                            {uploading ? "Uploading" : "Start Upload"}
+                        </Button>
+                        <Button onClick={handleCancelImport}>Hủy</Button>
+                    </Modal>
 
-    //                 <Space>
+                    <Space>
 
-    //                 </Space>
-    //                 <Space className="_right">
-    //                     {checkShowBtnDelete()}
+                    </Space>
+                    <Space className="_right">
+                        {checkShowBtnDelete()}
 
-    //                     {btnIndex()}
+                        {btnIndex()}
 
-    //                     {checkShowBtnExcel()}
-    //                 </Space>
+                        {checkShowBtnExcel()}
+                    </Space>
 
-    //                 {/* page name */}
-    //                 <b className="title-page">Meeting.</b>
+                    {/* page name */}
+                    <b className="title-page">Meeting.</b>
 
-    //                 {/* Show số lượng item/page */}
-    //                 <em> ( Trang {props.pageConfig.currentPage}, hiển thị{" "}
-    //                     {props.pageConfig.count}/{props.pageConfig.total} )
-    //                 </em>
-    //             </div>
+                    {/* Show số lượng item/page */}
+                    <em> ( Trang {props.pageConfig.currentPage}, hiển thị{" "}
+                        {props.pageConfig.count}/{props.pageConfig.total} )
+                    </em>
+                </div>
 
-    //             <hr />
-    //             <br />
+                <hr />
+                <br />
 
-    //             <Row>
-    //                 <Col sm={{ span: 7 }}>
-    //                     {searchLeft()}
-    //                 </Col>
-    //                 <Col sm={{ span: 17 }}>
-    //                     <Table
-    //                         size="small"
-    //                         // scroll={{ x: 1500, y: 7000 }}
-    //                         components={{
-    //                             body: {
-    //                                 cell: EditableCell,
-    //                             },
-    //                         }}
-    //                         loading={loadingTable}
-    //                         pagination={tableParams.pagination}
-    //                         // dataSource={formatData(dataSource)}
-    //                         dataSource={dataSource}
-    //                         columns={columns2}
-    //                         rowSelection={rowSelection}
-    //                         // rowClassName="editable-row"
-    //                         // className="table-index"
-    //                         expandable={{
-    //                             expandedRowRender,
-    //                             defaultExpandedRowKeys: ['1'],
-    //                         }}
+                <Row>
+                    <Col sm={{ span: 7 }}>
+                        {searchLeft()}
+                    </Col>
+                    <Col sm={{ span: 17 }}>
+                        <Table
+                            size="small"
+                            // scroll={{ x: 1500, y: 7000 }}
+                            components={{
+                                body: {
+                                    cell: EditableCell,
+                                },
+                            }}
+                            loading={loadingTable}
+                            pagination={tableParams.pagination}
+                            // dataSource={formatData(dataSource)}
+                            dataSource={dataSource}
+                            columns={columns2}
+                            rowSelection={rowSelection}
+                            // rowClassName="editable-row"
+                            // className="table-index"
+                            expandable={{
+                                expandedRowRender,
+                                defaultExpandedRowKeys: ['1'],
+                            }}
 
-    //                     />
-    //                 </Col>
-    //             </Row>
-    //         </Form>
-    //     </div>
-    // );
+                        />
+                    </Col>
+                </Row>
+            </Form>
+        </div>
+    );
 
     return (
         <div>
@@ -1390,9 +1390,9 @@ export default function meeting(props: any) {
                             </Form>
                         </Modal>
 
-                        {/* {pageContent} */}
+                        {pageContent}
 
-                        <Drawer
+                        {/* <Drawer
                             title="Chi tiết dự án"
                             placement="right"
                             open={openProjectDetail}
@@ -1476,7 +1476,7 @@ export default function meeting(props: any) {
 
                             <br />
 
-                        </Drawer>
+                        </Drawer> */}
                     </div>
                 }
             />
