35c6a67f8982dfe1a744c58b737a16fc30e16566
TienLQ <quangtienvkt@gmail.com>
2025-10-29 17:44:25 +0700

    OK

--- aio12/resources/js/Pages/Admin/Task/index_kanban copy.tsx
+++ aio12/resources/js/Pages/Admin/Task/index_kanban copy.tsx
@@ -140,69 +140,69 @@ export default function Dashboard(props: any) {
     //     return result;
     // }
 
-    // const onDragEnd = async (result: import("react-beautiful-dnd").DropResult) => {
-    //     const { source, destination } = result;
-    //     if (!destination) return;
+    const onDragEnd = async (result: import("react-beautiful-dnd").DropResult) => {
+        const { source, destination } = result;
+        if (!destination) return;
 
-    //     if (source.droppableId === destination.droppableId && source.index === destination.index) {
-    //         return;
-    //     }
+        if (source.droppableId === destination.droppableId && source.index === destination.index) {
+            return;
+        }
 
-    //     // Sao chép mảng gốc
-    //     const newDatas = [...columns];
+        // Sao chép mảng gốc
+        const newDatas = [...columns];
 
-    //     // lấy index của cột
-    //     const source_index = columns.findIndex(item => item.status.id === +source.droppableId);
-    //     const destination_index = columns.findIndex(item => item.status.id === +destination.droppableId);
+        // lấy index của cột
+        const source_index = columns.findIndex(item => item.status.id === +source.droppableId);
+        const destination_index = columns.findIndex(item => item.status.id === +destination.droppableId);
 
-    //     // data cần di chuyển
-    //     const itemToMove = newDatas[source_index].datas[source.index];
+        // data cần di chuyển
+        const itemToMove = newDatas[source_index].datas[source.index];
 
-    //     // trường hợp sắp xếp thứ tự trên cùng 1 cột
-    //     if (source.droppableId === destination.droppableId) {
-    //         // sắp xếp lại thứ tự của data
-    //         newDatas[source_index].datas = moveItemInArray(newDatas[source_index].datas, source.index, destination.index);
+        // trường hợp sắp xếp thứ tự trên cùng 1 cột
+        if (source.droppableId === destination.droppableId) {
+            // sắp xếp lại thứ tự của data
+            newDatas[source_index].datas = moveItemInArray(newDatas[source_index].datas, source.index, destination.index);
 
-    //         // save 2 state
-    //         setColumns(newDatas); // Cập nhật state
+            // save 2 state
+            setColumns(newDatas); // Cập nhật state
 
-    //         // lọc ra ids là danh sách id của data sau khi đã sắp xếp
-    //         const ids = newDatas[destination_index].datas.map(item => item.id);
-    //         // save 2 db
-    //         await updateTask(itemToMove.id, {
-    //             task_status_id: destination.droppableId,
-    //             ids: ids,
-    //             parentName: props.parentName,
-    //             pid: props.pid,
-    //         });
+            // lọc ra ids là danh sách id của data sau khi đã sắp xếp
+            const ids = newDatas[destination_index].datas.map(item => item.id);
+            // save 2 db
+            await updateTask(itemToMove.id, {
+                task_status_id: destination.droppableId,
+                ids: ids,
+                parentName: props.parentName,
+                pid: props.pid,
+            });
 
-    //         return;
-    //     }
+            return;
+        }
 
-    //     // trường hợp di chuyển sang cột khác
+        // trường hợp di chuyển sang cột khác
 
-    //     // Xoá khỏi vị trí cũ
-    //     newDatas[source_index].datas = newDatas[source_index].datas.filter((_, i) => i !== source.index);
+        // Xoá khỏi vị trí cũ
+        newDatas[source_index].datas = newDatas[source_index].datas.filter((_, i) => i !== source.index);
 
-    //     // Thêm vào vị trí mới
-    //     newDatas[destination_index].datas = [...newDatas[destination_index].datas, itemToMove];
-    //     // sắp xếp lại thứ tự của data
-    //     const lastIndex = newDatas[destination_index].datas.length - 1;
-    //     newDatas[destination_index].datas = moveItemInArray(newDatas[destination_index].datas, lastIndex, destination.index);
+        // Thêm vào vị trí mới
+        newDatas[destination_index].datas = [...newDatas[destination_index].datas, itemToMove];
+        // sắp xếp lại thứ tự của data
+        const lastIndex = newDatas[destination_index].datas.length - 1;
+        newDatas[destination_index].datas = moveItemInArray(newDatas[destination_index].datas, lastIndex, destination.index);
 
 
-    //     // Cập nhật state
-    //     setColumns(newDatas);
+        // Cập nhật state
+        setColumns(newDatas);
 
-    //     // lọc ra ids là danh sách id của data sau khi đã sắp xếp
-    //     const ids = newDatas[destination_index].datas.map(item => item.id);
+        // lọc ra ids là danh sách id của data sau khi đã sắp xếp
+        const ids = newDatas[destination_index].datas.map(item => item.id);
 
-    //     await updateTask(itemToMove.id, {
-    //         task_status_id: destination.droppableId,
-    //         ids: ids
-    //     });
+        await updateTask(itemToMove.id, {
+            task_status_id: destination.droppableId,
+            ids: ids
+        });
 
-    // };
+    };
 
     function initialValuesForm() {
         return search;
