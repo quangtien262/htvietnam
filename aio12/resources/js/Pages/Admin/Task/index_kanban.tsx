e658083d9fe44970a6a1eec459427efbf4a90823
TienLQ <quangtienvkt@gmail.com>
2025-10-29 14:13:10 +0700

    OK

--- aio12/resources/js/Pages/Admin/Task/index_kanban.tsx
+++ aio12/resources/js/Pages/Admin/Task/index_kanban.tsx
@@ -758,44 +758,7 @@ export default function Dashboard(props: any) {
                                 onClose={() => setOpenDetail(false)}
                                 width="90%"
                             >
-                                {taskInfo(props,
-                                    users, 
-                                    dataAction,
-                                    comments,
-                                    checklist,
-                                    checklistPercent,
-                                    taskLog,
-                                    priority,
-                                    status,
-                                    (result: any) => {
-                                        // set columns, dùng cho case fast edit
-                                        if (result.columns) {
-                                            setColumns(result.columns);
-                                        }
-
-                                        // set data action, dùng cho case fast edit
-                                        if (result.dataAction_column) {
-                                            setDataAction({
-                                                ...((typeof dataAction === 'object' && dataAction !== null) ? dataAction : {}),
-                                                [result.dataAction_column.col]: result.dataAction_column.val,
-                                            });
-                                        }
-
-                                        // set checklist
-                                        if (result.checklist) {
-                                            setChecklist(result.checklist);
-                                        }
-
-                                        // set percent
-                                        if (result.checklist_percent !== undefined) {
-                                            setChecklistPercent(result.checklist_percent);
-                                        }
-                                        // set comments
-                                        if (result.comments) {
-                                            setComments(result.comments);
-                                        }
-
-                                    })}
+                               
 
                                 <br />
 
